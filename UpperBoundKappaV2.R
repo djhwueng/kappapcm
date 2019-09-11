9864bb060535df58d3554009e1fe55c62b8f9d46
Tony Jhwueng <djhwueng@umail.iu.edu>
2019-09-11 08:07:01 +0800

    Add files via upload

--- UpperBoundKappaV2.R
+++ UpperBoundKappaV2.R
@@ -59,12 +59,57 @@ for(treesizeIndex in 1:length(treesize.array)){
 }
 
 save.image("upperboundkappa.rda")
+
+
+
 par(mfrow=c(2,2))
 plot(tree50[1,],tree50[2,])
 plot(tree100[1,],tree100[2,])
 plot(tree500[1,],tree500[2,])
 plot(tree800[1,],tree800[2,])
+colnames(tree50)<-colnames(tree100)<-colnames(tree500)<-colnames(tree800)<-NULL
+o50<-order(tree50[1,])
+tree50[1,]
+tree50<-tree50[,o50]
+tree50[1,]
+tree50[2,]
+o100<-order(tree100[1,])
+tree100<-tree100[,o100]
+o500<-order(tree500[1,])
+tree500<-tree500[,o500]
+o800<-order(tree800[1,])
+tree800<-tree800[,o800]
 
+
+merge50<-array(NA,c(2,10))
+merge100<-array(NA,c(2,10))
+merge500<-array(NA,c(2,10))
+merge800<-array(NA,c(2,10))
+
+for(i in 1:10){
+  merge50[1,i]<-mean(tree50[1,((i-1)*4+1):(i*4)])
+  merge50[2,i]<-mean(tree50[2,((i-1)*4+1):(i*4)])
+  merge100[1,i]<-mean(tree100[1,((i-1)*4+1):(i*4)])
+  merge100[2,i]<-mean(tree100[2,((i-1)*4+1):(i*4)])
+  merge500[1,i]<-mean(tree500[1,((i-1)*4+1):(i*4)])
+  merge500[2,i]<-mean(tree500[2,((i-1)*4+1):(i*4)])
+  merge800[1,i]<-mean(tree800[1,((i-1)*4+1):(i*4)])
+  merge800[2,i]<-mean(tree800[2,((i-1)*4+1):(i*4)])
+}
+
+library(ggplot2)
+df<-data.frame( log10kappa=c(merge50[1,],merge100[1,],merge500[1,],merge800[1,]),
+                Proportion=c(1-merge50[2,],1-merge100[2,],1-merge500[2,],1-merge800[2,]), 
+                Taxa=c(rep(50,dim(merge50)[2]),rep(100,dim(merge50)[2]),rep(500,dim(merge50)[2]),rep(800,dim(merge50)[2]))
+)
+
+df$Taxa<-as.factor(df$Taxa)
+p.plot<-ggplot(df,aes(x=log10kappa,y=Proportion,col=Taxa,group=Taxa))+ geom_point() + geom_line(size=2)
+p.plot<- p.plot + ggtitle(label = "Solvable C Proportion") + labs(x=expression(paste(log[10], " ", kappa))) #+ xlim(14,20) 
+p.plot<-p.plot+theme(
+  plot.title = element_text(color = "black", size = 20, face = "bold"))+ theme(plot.title = element_text(hjust = 0.5))
+
+p.plot
 
 #?apply
 #unsoltree<-apply(sol.array[1,,],1,is.na)
